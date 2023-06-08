be46697a8455635bbb5cf1d289cc14853c64589a
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-08 15:14:24 +0200

    add technology controller migration and seeder

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -93,6 +93,8 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $type->delete();
+
+        return to_route('admin.types.index')->with('message', 'Type Deleted!');
     }
 }
---
+++ config/db_technologies.php
@@ -0,0 +1,28 @@
+<?php 
+
+return [
+    [
+        'name' => 'HTML',
+    ],
+    [
+        'name' => 'CSS',
+    ],
+    [
+        'name' => 'SCSS',
+    ],
+    [
+        'name' => 'Javascript',
+    ],
+    [
+        'name' => 'Vue JS',
+    ],
+    [
+        'name' => 'PHP',
+    ],
+    [
+        'name' => 'Laravel',
+    ],
+    [
+        'name' => 'My Sql',
+    ],
+];
